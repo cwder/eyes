7a6c447b6bd2911de5e03e30b7b1d869ce3d546a
chinapnr <wenda.cao@huifu.com>
2020-07-02 22:49:23 +0800

    加入url

--- action/spider.py
+++ action/spider.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "http://quote.eastmoney.com/center/gridlist.html#hs_a_board"
-
+# 东方财富网个股
+url2 = "http://55.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112407838904080399163_1593699026973&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:13,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1593699027081"
 
 class Spider:
 
@@ -24,7 +25,7 @@ class Spider:
 
         requests.adapters.DEFAULT_RETRIES = 5
         try:
-            response = requests.get(url, timeout=10, headers=headers)
+            response = requests.get(url2, timeout=10, headers=headers)
             response.encoding = 'utf8'
             html = response.text
             soup = BeautifulSoup(html, "html.parser")
