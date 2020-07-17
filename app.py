0bd361e75c5796e7bd49f7f61949ac1d2ee6d7f1
chinapnr <wenda.cao@huifu.com>
2020-07-18 00:01:25 +0800

    上云部署

---
+++ app.py
@@ -0,0 +1,16 @@
+# from fastapi import FastAPI
+#
+# from spider.shares_spider import Share
+#
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# def health():
+#     return {"love you": "my eyes"}
+#
+#
+# if __name__ == '__main__':
+#     info = Share()
+#     info.work()
+print("docker---start----")
--- main.py
+++
@@ -1,6 +0,0 @@
-from spider.shares_spider import Share
-
-if __name__ == '__main__':
-    info = Share()
-    info.work()
-
