71d79d022464159ad777b3f2146ac54a93c47be9
chinapnr <wenda.cao@huifu.com>
2020-07-27 22:09:48 +0800

    auto

--- app.py
+++ app.py
@@ -14,8 +14,12 @@ app = FastAPI()
 
 @app.get("/")
 def health():
-    info = Share()
-    info.work()
+    logger.info("health------------")
+    try:
+        info = Share()
+        info.work()
+    except Exception as ex:
+        logger.info("health--ex---------- " + str(ex))
     a = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     return {"love you": "my and you eyes test " + a}
 
