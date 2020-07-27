340568e6673f19a9583ffb48bc8788a7b84bd652
chinapnr <wenda.cao@huifu.com>
2020-07-27 21:52:25 +0800

    auto

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ from fishbase import logger
 
 from config.config import EyesConfig
 from schedule.daycheduler import scheduler
+from spider.shares_spider import Share
 
 app = FastAPI()
 
 
 @app.get("/")
 def health():
+    info = Share()
+    info.work()
     a = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     return {"love you": "my and you eyes test " + a}
 
