b4633da4f8625e71c6edcdca9c79db3c558507d1
chinapnr <wenda.cao@huifu.com>
2020-08-04 22:37:50 +0800

    auto

--- schedule/daycheduler.py
+++ schedule/daycheduler.py
@@ -4,13 +4,14 @@ from fishbase import logger
 from config.config import EyesConfig
 from spider.old_shares_spider import OldShare
 from spider.shares_spider import Share
+from spider.t_shares_spider import OldAShare
 
 
 def job():
     EyesConfig.initLog()
-    info = OldShare()
+    info = OldAShare()
     info.work()
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(job, 'cron', hour=22, minute=28)
+scheduler.add_job(job, 'cron', hour=22, minute=40)
--- spider/t_shares_spider.py
+++ spider/t_shares_spider.py
@@ -10,7 +10,7 @@ from create_db import Base, engine, Session
 from spider.base_spider import BaseSpider
 
 
-class OldShare(BaseSpider):
+class OldAShare(BaseSpider):
 
     def get_detail_url(self, page):
         detail_url = "http://55.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112407838904080399163_1593699026973&pn=%d&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:13,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1593699027081" % page
@@ -85,5 +85,5 @@ class OldShare(BaseSpider):
 
 
 if __name__ == '__main__':
-    info = OldShare()
+    info = OldAShare()
     info.work()
