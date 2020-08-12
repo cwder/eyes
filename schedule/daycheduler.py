9072688efb40545c75b85bf3d1c96f188e0a0664
chinapnr <wenda.cao@huifu.com>
2020-08-12 23:26:12 +0800

    auto

--- schedule/daycheduler.py
+++ schedule/daycheduler.py
@@ -8,9 +8,9 @@ from spider.old_shares_spider2 import OldShare2
 
 def job():
     EyesConfig.initLog()
-    info = OldShare2()
+    info = EyesShare()
     info.work()
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(job, 'cron', hour=23, minute=8)
+scheduler.add_job(job, 'cron', hour=23, minute=30)
