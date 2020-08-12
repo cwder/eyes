8936a0dc327000604b264fc7caedddc10bc16440
wenda.cao <wenda.cao@huifu.com>
2020-08-12 09:48:04 +0800

    auto

--- schedule/daycheduler.py
+++ schedule/daycheduler.py
@@ -2,14 +2,14 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from config.config import EyesConfig
 from spider.final_spider import EyesShare
+from spider.old_shares_spider import OldShare
 
 
 def job():
     EyesConfig.initLog()
-    info = EyesShare()
-    info.run()
-    info.run()
+    info = OldShare()
+    info.work()
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(job, 'cron', hour=23, minute=40)
+scheduler.add_job(job, 'cron', hour=10, minute=0)
