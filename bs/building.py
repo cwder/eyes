c376d108066237bff56ec2ff13dda0ff747ec8ac
chinapnr <wenda.cao@huifu.com>
2020-10-10 23:30:41 +0800

    ok

--- bs/building.py
+++ bs/building.py
@@ -58,7 +58,7 @@ class Build:
                     session.execute(sql)
                     allTables.append(tableName)
         for tableName in allTables:
-            if 'zygote' in tableName:
+            if not tableName.startswith('sz') or not tableName.startswith('sh'):
                 continue
             tName = Utils.formatTableName(tableName)
             sql = "select * from {} order by date desc".format(tName)
